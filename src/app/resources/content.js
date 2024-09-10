41a003f1d684594e203c36a40ccfb7baf57ea714
Mujtaba Hassan <mujtabahassanrizvi@outlook.com>
2024-09-10 17:03:47 -0400

    experimentally added letterfx

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -53,7 +53,32 @@ const home = {
     label: 'Home',
     title: `${person.name}'s Portfolio`,
     description: `Portfolio website showcasing my work as a ${person.role}`,
-    headline: <>Freelance Digital Strategy Director</>,
+    headline: <span
+    style={{
+      fontFamily: 'var(--font-primary)'
+    }}
+  >
+    <LetterFx
+      speed="medium"
+      trigger="hover"
+      charset={[
+        'X',
+        '@',
+        '$',
+        'a',
+        'H',
+        'z',
+        'o',
+        '0',
+        'y',
+        '#',
+        '?',
+        '*',
+        '0',
+        '1',
+        '+'
+      ]}
+    >Freelance Digital Strategy Director</LetterFx></span>,
     subline: <>Dedicated and diligent Freelance Digital Strategy Director & Web Developer with <br/><InlineCode>4+</InlineCode> years of experience in overseeing website development, enhancing user support, and utilizing tools & programming languages including Blender & Python. Proficient in implementing engineering principles, creating software solutions, analyzing business & user requirements, and drafting technical documentation.</>
 }
 
