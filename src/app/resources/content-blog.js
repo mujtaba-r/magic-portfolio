582ca42c24c2e8c0b068e0657706eb06923b9b09
Mujtaba Hassan <mujtabahassanrizvi@outlook.com>
2024-09-10 00:08:20 -0400

    fixed timezones

--- src/app/resources/content-blog.js
+++ src/app/resources/content-blog.js
@@ -9,7 +9,7 @@ const person = {
     },
     role:      'Software Engineer',
     avatar:    '/images/avatar.jpg',
-    location:  'America/Canada',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    location:  'America/Toronto',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: ['English', 'Urdu', 'Hindi']  // optional: Leave the array empty if you don't want to display languages
 }
 
