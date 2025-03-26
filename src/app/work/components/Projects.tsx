6e89fbd2bdd6da5e16e1ecc6278a19b0bfc693e8
Mujtaba Hassan <mujtabahassanrizvi@outlook.com>
2025-03-26 13:18:11 -0400

    fixed bugs

--- src/app/work/components/Projects.tsx
+++ src/app/work/components/Projects.tsx
@@ -1,6 +1,5 @@
 import { getPosts } from '@/app/utils';
 import { Flex } from '@/once-ui/components';
-
 import { ProjectCard } from '@/app/components';
 
 interface ProjectsProps {
@@ -25,12 +24,9 @@ export function Projects({ range }: ProjectsProps) {
             {displayedProjects.map((post) => (
                 <ProjectCard
                     key={post.slug}
-                    href={`/work/${post.slug}`}
-                    images={post.metadata.images}
-                    title={post.metadata.title}
-                    description={post.metadata.summary}
-                    content={post.content}
-                    avatars={post.metadata.team?.map((member) => ({ src: member.avatar })) || []}/>
+                    metadata={post.metadata}
+                    slug={post.slug}
+                />
             ))}
         </Flex>
     );
