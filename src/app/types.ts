c8d5283c4e5a638ed54ecfc668aad1c3ff2043ab
Mujtaba Hassan <mujtabahassanrizvi@outlook.com>
2025-03-26 22:17:30 -0400

    fixed type def errors

--- src/app/types.ts
+++ src/app/types.ts
@@ -5,4 +5,31 @@ export interface ProjectMetadata {
     tags: string[];
     publishedAt: string;
     updatedAt?: string;
+}
+
+export interface Image {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+}
+
+export interface Experience {
+    company: string;
+    role: string;
+    description: string;
+    startDate: string;
+    endDate?: string;
+    images?: Image[];
+}
+
+export interface Institution {
+    name: string;
+    description: string;
+}
+
+export interface Skill {
+    title: string;
+    description: string;
+    images?: Image[];
 } 
