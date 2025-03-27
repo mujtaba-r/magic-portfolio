54441485264cd38f017c13a68d822c7130a5218c
Mujtaba Hassan <mujtabahassanrizvi@outlook.com>
2025-03-27 14:20:07 -0400

    added style to table of contents

--- src/app/about/components/TableOfContents.tsx
+++ src/app/about/components/TableOfContents.tsx
@@ -78,6 +78,7 @@ const TableOfContents: React.FC<TableOfContentsProps> = ({ structure, about }) =
                                         background="neutral-strong">
                                     </Flex>
                                     <Text>
+                                        <style>fontFamily: 'var(--font-secondary)'</style>
                                         {item}
                                     </Text>
                                 </Flex>
